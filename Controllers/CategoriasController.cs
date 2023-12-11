ef43ab9fb0a5e4c01e918ab4a7ed0bfe392097fa
GocasPT <gocasousa2013@gmail.com>
2023-12-11 10:23:40 +0000

    [UPDATE] Categoria Views + Controller

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -59,7 +59,7 @@ namespace HabitAqui.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Descricao")] Categoria categoria)
+        public async Task<IActionResult> Create([Bind("Id,Nome,Descricao,Ativo")] Categoria categoria)
         {
             if (ModelState.IsValid)
             {
@@ -91,7 +91,7 @@ namespace HabitAqui.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao")] Categoria categoria)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao,Ativo")] Categoria categoria)
         {
             if (id != categoria.Id)
             {
@@ -119,43 +119,6 @@ namespace HabitAqui.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(categoria);
-        }
-
-        // GET: Categorias/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categorias == null)
-            {
-                return NotFound();
-            }
-
-            var categoria = await _context.Categorias
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (categoria == null)
-            {
-                return NotFound();
-            }
-
-            return View(categoria);
-        }
-
-        // POST: Categorias/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Categorias == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Categorias'  is null.");
-            }
-            var categoria = await _context.Categorias.FindAsync(id);
-            if (categoria != null)
-            {
-                _context.Categorias.Remove(categoria);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
