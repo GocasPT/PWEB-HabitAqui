e6d7410479d4ce504f595b180551665fef047294
brunuh <a2019132612@isec.pt>
2023-12-13 18:20:31 +0000

    date of creation when creating users is now saved

---
+++ Data/Migrations/20231213180421_AddDataCriacao.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitAqui.Migrations
+{
+    public partial class AddDataCriacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true,
+                defaultValue: DateTime.UtcNow);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "AspNetUsers");
+        }
+    }
+}
